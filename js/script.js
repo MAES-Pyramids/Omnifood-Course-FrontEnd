0cb806cbbe7433dd8a3881b16b6c992df1c576a2
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2022-11-03 17:29:20 +0200

    Browser Support and Fixing Flexbox Gap in Safari

--- js/script.js
+++ js/script.js
@@ -63,9 +63,26 @@ const observer = new IntersectionObserver(
   {
     root: null,
     threshold: 0,
-    rootMargin: "-85px",
+    rootMargin: "-80px",
   }
 );
 observer.observe(sectionHeader);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
