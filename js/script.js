ce3815fa2560fd2cc074f9e80fbcafbea299bb43
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2022-11-03 14:03:04 +0200

    Making the Mobile Navigation Work

--- js/script.js
+++ js/script.js
@@ -6,6 +6,24 @@ yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+const navLinks = document.querySelectorAll(".main-nav-link");
+
+btnNav.addEventListener("click", (e) => {
+  e.preventDefault();
+  header.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    if (link.className == "main-nav-link main-nav-link-cta") {
+      e.preventDefault();
+    }
+
+    header.classList.remove("nav-open");
+  });
+});
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
