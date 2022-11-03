775d526b1220e7f71c578e7815e875a2d6e7d40b
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2022-11-03 15:28:25 +0200

    Implementing Smooth Scrolling manually

--- js/script.js
+++ js/script.js
@@ -8,25 +8,52 @@ yearEl.textContent = currentYear;
 // Make mobile navigation work
 const btnNav = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
-const navLinks = document.querySelectorAll(".main-nav-link");
 
 btnNav.addEventListener("click", (e) => {
   e.preventDefault();
   header.classList.toggle("nav-open");
 });
 
-navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    if (link.className == "main-nav-link main-nav-link-cta") {
-      e.preventDefault();
-    }
+// const navLinks = document.querySelectorAll(".main-nav-link");
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", (e) => {
+// if (link.classList.contains("main-nav-link-cta")) {
+//   e.preventDefault();
+// }
 
-    header.classList.remove("nav-open");
-  });
-});
+//     header.classList.remove("nav-open");
+//   });
+// });
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href == "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      if (!link.classList.contains("main-nav-link-cta")) {
+        const section = document.querySelector(href);
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+
+    // Close mobile navigation
+
+    if (link.classList.contains("main-nav-link"))
+      header.classList.remove("nav-open");
+  });
+});
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
